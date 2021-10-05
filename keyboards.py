0e120577dcf2555ae6b2608fd5ec2514d677930c
Dave Akhmadeev <79203081+Tekorukite@users.noreply.github.com>
2021-10-05 19:07:10 +0300

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,73 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+    InlineKeyboardButton
+
+inline_cancel = InlineKeyboardButton('В начало', callback_data='cancel')
+inline_back = InlineKeyboardButton('Назад', callback_data='back')
+inline_credit_card = InlineKeyboardButton('Картой онлайн', callback_data='credit_card')
+inline_sb_online = InlineKeyboardButton('Сбер Онлайн', callback_data='sb_online')
+inline_sb_atm = InlineKeyboardButton('Сбер терминал', callback_data='sb_atm')
+inline_vtb_atm = InlineKeyboardButton('ВТБ терминал', callback_data='vtb_atm')
+inline_faq = InlineKeyboardButton('Назад', callback_data='faq')
+inline_send = InlineKeyboardButton('Отправить', callback_data='send')
+inline_edit = InlineKeyboardButton('Редактировать', callback_data='edit')
+inline_dorm = InlineKeyboardButton('Общежитие', callback_data='edit_dorm')
+inline_name = InlineKeyboardButton('Имя', callback_data='edit_name')
+inline_building = InlineKeyboardButton('Корпус', callback_data='edit_building')
+inline_room = InlineKeyboardButton('Комната', callback_data='edit_room')
+inline_login = InlineKeyboardButton('Логин', callback_data='edit_login')
+inline_phone = InlineKeyboardButton('Телефон', callback_data='edit_phone')
+inline_problem = InlineKeyboardButton('Проблема', callback_data='edit_problem')
+inline_call_time = InlineKeyboardButton('Время звонка', callback_data='edit_call_time')
+button_faq = KeyboardButton('FAQ')
+button_router = KeyboardButton('Настройка роутера')
+button_payment = KeyboardButton('Оплата')
+button_support = KeyboardButton('Заявка в техподдержку')
+button_ds = KeyboardButton('ГЗ')
+button_dsl = KeyboardButton('ДСЛ')
+button_fds = KeyboardButton('ФДС')
+button_dsv = KeyboardButton('ДСВ')
+button_dsk = KeyboardButton('ДСК')
+button_dssh = KeyboardButton('ДСШ')
+button_dsy = KeyboardButton('ДСЯ')
+button_cancel = KeyboardButton('Отмена')
+
+inline_edit_kb = InlineKeyboardMarkup(row_width=2)
+inline_edit_kb.add(inline_dorm, inline_building,
+                   inline_room, inline_name,
+                   inline_phone, inline_login,
+                   inline_problem, inline_call_time,
+                   inline_back)
+
+dorm_kb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=4, one_time_keyboard=True)
+dorm_kb.add(button_ds, button_dsl, button_fds, button_dsv, button_dsk, button_dsy, button_dssh)
+dorm_kb.add(button_cancel)
+
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+start_kb.add(button_payment, button_router, button_faq, button_support)
+
+inline_kb_payment = InlineKeyboardMarkup(row_width=2)
+inline_kb_payment.add(inline_credit_card, inline_sb_online, inline_sb_atm, inline_vtb_atm, inline_cancel)
+
+inline_kb_router = InlineKeyboardMarkup(row_width=1)
+inline_kb_router.add(InlineKeyboardButton('Настройка роутера', url='https://msu.umos.ru/routers.html'), inline_cancel)
+
+inline_faq_kb_1 = InlineKeyboardMarkup(row_width=1)
+inline_faq_kb_1.add(InlineKeyboardButton('Куда обращаться?', callback_data='faq_0'))
+inline_faq_kb_1.add(InlineKeyboardButton('Сколько стоит?', callback_data='faq_1'))
+inline_faq_kb_1.add(InlineKeyboardButton('Как оплачивать?', callback_data='faq_2'))
+inline_faq_kb_1.add(InlineKeyboardButton('Заблокировали за неуплату.', callback_data='faq_3'))
+inline_faq_kb_1.add(
+    InlineKeyboardButton('Интернет отключили \nменьше чем через месяц \nпосле оплаты?', callback_data='faq_4'))
+inline_faq_kb_1.add(InlineKeyboardButton('Следующая страница ▶️', callback_data='next_page'))
+inline_faq_kb_1.add(inline_cancel)
+
+inline_faq_kb_2 = InlineKeyboardMarkup(row_width=1)
+inline_faq_kb_2.add(
+    InlineKeyboardButton('Появилась необходимость \nуехать на длительное время.', callback_data='faq_5'))
+inline_faq_kb_2.add(InlineKeyboardButton('Как мне поменять тариф?', callback_data='faq_6'))
+inline_faq_kb_2.add(InlineKeyboardButton('Что делать при переезде?', callback_data='faq_7'))
+inline_faq_kb_2.add(InlineKeyboardButton('Как расторгнуть договор?', callback_data='faq_8'))
+inline_faq_kb_2.add(
+    InlineKeyboardButton('Интернет работает \nтолько на одном устройстве.', callback_data='faq_9'))
+inline_faq_kb_2.add(InlineKeyboardButton('◀️ Предыдущая страница', callback_data='prev_page'))
+inline_faq_kb_2.add(inline_cancel)
---
+++ main.py
@@ -0,0 +1,376 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import keyboards
+from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import texts
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+    InlineKeyboardButton
+from aiogram.utils.exceptions import MessageNotModified
+from contextlib import suppress
+from aiogram.dispatcher.filters import Text
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import requests
+
+storage = MemoryStorage()
+
+WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}"
+WEBHOOK_HOST = f"https://{HEROKU_APP_NAME}.herokuapp.com"
+WEBHOOK_PATH = f"/{TELEGRAM_TOKEN}"
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = PORT
+POSSIBLE_DORMITORY_NAMES = ['ГЗ', 'ДСЛ', 'ФДС', 'ДСВ', 'ДСК', 'ДСШ', 'ДСЯ']
+DS_GFORM = {
+    'url': 'https://docs.google.com/forms/d/e/1FAIpQLSdPxSyJxmyuoyidT5vPx4_cD53118OcT7j5Qv-sOiNz2G03mg/formResponse',
+    'name': 'entry.651748368',
+    'phone': 'entry.539972922',
+    'login': 'entry.1963659940',
+    'room': 'entry.234720864',
+    'problem': 'entry.835582122',
+    'time': 'entry.681152745'
+}
+DSL_GFORM = {
+    'url': 'https://docs.google.com/forms/d/e/1FAIpQLSe_2_hHZQHAz1x1u5rbMd51nY4ruWBNWib5QODlCCtT_Qtphg/formResponse',
+    'name': 'entry.315753433',
+    'phone': 'entry.329797146',
+    'login': 'entry.782191196',
+    'room': 'entry.100962389',
+    'problem': 'entry.1401337801',
+    'time': 'entry.97627762',
+    'building': 'entry.1452465197',
+    'building_other': 'entry.1452465197.other_option_response'
+}
+FDS_GFORM = {
+    'url': 'https://docs.google.com/forms/d/e/1FAIpQLSeMIc5TuwUiegFx7BWHXJrPjbm1HP-Gefsgvx_BKi2WLxNPbg/formResponse',
+    'name': 'entry.315753433',
+    'phone': 'entry.329797146',
+    'login': 'entry.782191196',
+    'room': 'entry.100962389',
+    'problem': 'entry.1401337801',
+    'time': 'entry.97627762',
+    'building': 'entry.1452465197',
+    'building_other': 'entry.1452465197.other_option_response'
+}
+DSVKSY_GFORM = {
+    'url': 'https://docs.google.com/forms/d/e/1FAIpQLSfjy_GB8kMNn7zQZL7KeJPhqiGhhoq42IfLgybkbtXeNdaTIA/formResponse',
+    'name': 'entry.315753433',
+    'phone': 'entry.329797146',
+    'login': 'entry.782191196',
+    'dorm': 'entry.1452465197',
+    'room': 'entry.100962389',
+    'problem': 'entry.1401337801',
+    'time': 'entry.97627762'
+}
+
+
+class Support(StatesGroup):
+    faq = State()
+    payment = State()
+    dormitory = State()
+    building = State()
+    room = State()
+    name = State()
+    phone = State()
+    login = State()
+    problem = State()
+    call_time = State()
+    filled = State()
+    edit = State()
+
+
+bot = Bot(token=TELEGRAM_TOKEN, parse_mode='markdownv2')
+dp = Dispatcher(bot, storage=storage)
+logging.basicConfig(level=logging.DEBUG)
+
+
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer(f'Привет, {message.from_user.first_name}\. Я бот техподдержки ЮМОС\. \n'
+                         f'Какой вопрос Вас интересует?',
+                         reply_markup=keyboards.start_kb)
+
+
+@dp.message_handler(commands="payment")
+@dp.message_handler(lambda message: message.text.lower() == 'оплата')
+async def cmd_payment(message: types.Message):
+    await Support.payment.set()
+    await message.answer(f"Каким способом оплаты Вы желаете воспользоваться?\n",
+                         reply_markup=keyboards.inline_kb_payment)
+
+
+@dp.callback_query_handler(text='credit_card', state=Support.payment)
+async def cmd_credit_card(call: types.CallbackQuery):
+    await call.message.edit_text(texts.CREDIT_CARD_TEXT, reply_markup=InlineKeyboardMarkup(row_width=1).add(
+        InlineKeyboardButton('Личный кабинет', url='https://msu.umos.ru/?module=01_login'), keyboards.inline_back,
+        keyboards.inline_cancel))
+
+
+@dp.callback_query_handler(text='sb_online', state=Support.payment)
+async def cmd_sb_online(call: types.CallbackQuery):
+    await call.message.edit_text(texts.SB_TEXT, reply_markup=InlineKeyboardMarkup(row_width=1).add(
+        InlineKeyboardButton('Подробная инструкция', url='https://msu.umos.ru/files/sberbank_online.pdf'),
+        keyboards.inline_back, keyboards.inline_cancel))
+
+
+@dp.callback_query_handler(text='sb_atm', state=Support.payment)
+async def cmd_sb_atm(call: types.CallbackQuery):
+    await call.message.edit_text(texts.SB_TEXT, reply_markup=InlineKeyboardMarkup(row_width=1).add(
+        InlineKeyboardButton('Подробная инструкция', url='https://msu.umos.ru/files/sberbank.pdf'),
+        keyboards.inline_back, keyboards.inline_cancel))
+
+
+@dp.callback_query_handler(text='vtb_atm', state=Support.payment)
+async def cmd_vtb_atm(call: types.CallbackQuery):
+    await call.message.edit_text(texts.VTB_TEXT, reply_markup=InlineKeyboardMarkup(row_width=1).add(
+        InlineKeyboardButton('Подробная инструкция', url='https://msu.umos.ru/files/vtb.pdf'), keyboards.inline_back,
+        keyboards.inline_cancel))
+
+
+@dp.callback_query_handler(text='back', state=Support.payment)
+async def cmd_back_payment(call: types.CallbackQuery):
+    await call.message.edit_text(f"Каким способом оплаты Вы желаете воспользоваться?\n",
+                                 reply_markup=keyboards.inline_kb_payment)
+
+
+@dp.message_handler(lambda message: message.text.lower() == 'отмена', state='*')
+async def cmd_cancel_button(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer(f'Какой вопрос Вас интересует?', reply_markup=keyboards.start_kb)
+
+
+@dp.callback_query_handler(text='cancel', state="*")
+async def cmd_cancel(call: types.CallbackQuery, state: FSMContext):
+    await state.finish()
+    with suppress(MessageNotModified):
+        await call.message.delete_reply_markup()
+        await call.answer()
+        await call.message.answer(f'Какой вопрос Вас интересует?', reply_markup=keyboards.start_kb)
+
+
+@dp.message_handler(commands="router")
+@dp.message_handler(lambda message: message.text.lower() == 'настройка роутера')
+async def cmd_router(message: types.Message):
+    await message.answer(f"Информацию о настройке роутера для работы в нашей сети можно найти по ссылке ниже\.",
+                         reply_markup=keyboards.inline_kb_router)
+
+
+@dp.message_handler(commands="faq", state='*')
+@dp.message_handler(lambda message: message.text.lower() == 'faq', state='*')
+async def cmd_faq(message: types.Message, state: FSMContext):
+    await Support.faq.set()
+    await state.update_data(current_faq_page=1)
+    await message.answer(f'Распространенные вопросы:\n', reply_markup=keyboards.inline_faq_kb_1)
+
+
+@dp.callback_query_handler(Text(startswith="faq_"), state=Support.faq)
+async def cmd_faq_question(call: types.CallbackQuery, state: FSMContext):
+    question = int(call.data[4:])
+    kb = InlineKeyboardMarkup(row_width=1)
+    if question == 1:
+        kb.add(InlineKeyboardButton('Тарифы', url='https://msu.umos.ru/?module=tariffs'))
+    elif question == 2:
+        kb.add(InlineKeyboardButton('Способы оплаты', url='https://msu.umos.ru/?module=oplata'))
+    elif question == 3:
+        kb.add(InlineKeyboardButton('Личный кабинет', url='https://msu.umos.ru/?module=01_login'))
+    elif question == 9:
+        kb.add(InlineKeyboardButton('Настройка роутера', url='https://msu.umos.ru/routers.html'))
+    kb.add(keyboards.inline_back, keyboards.inline_cancel)
+    await call.message.edit_text(texts.FAQ_Q[question] + texts.FAQ_Ans[question], reply_markup=kb)
+
+
+@dp.callback_query_handler(text='back', state=Support.faq)
+async def cmd_cb_faq(call: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    if user_data['current_faq_page'] == 1:
+        await call.message.edit_text(f'Распространенные вопросы:\n', reply_markup=keyboards.inline_faq_kb_1)
+    elif user_data['current_faq_page'] == 2:
+        await call.message.edit_text(f'Распространенные вопросы:\n', reply_markup=keyboards.inline_faq_kb_2)
+
+
+@dp.callback_query_handler(text='next_page', state=Support.faq)
+async def cmd_next(call: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    await state.update_data(current_faq_page=user_data['current_faq_page'] + 1)
+    await call.message.edit_reply_markup(keyboards.inline_faq_kb_2)
+
+
+@dp.callback_query_handler(text='prev_page', state=Support.faq)
+async def cmd_next(call: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    await state.update_data(current_faq_page=user_data['current_faq_page'] - 1)
+    await call.message.edit_reply_markup(keyboards.inline_faq_kb_1)
+
+
+@dp.message_handler(commands="support")
+@dp.message_handler(lambda message: message.text.lower() == 'заявка в техподдержку')
+async def cmd_support(message: types.Message):
+    await message.answer('Выберите общежитие:', reply_markup=keyboards.dorm_kb)
+    await Support.dormitory.set()
+
+
+@dp.message_handler(state=Support.dormitory)
+async def cmd_building(message: types.Message, state: FSMContext):
+    if message.text not in POSSIBLE_DORMITORY_NAMES:
+        await message.answer("Пожалуйста, выберите общежитие, используя клавиатуру ниже\.",
+                             reply_markup=keyboards.dorm_kb)
+        return
+    await state.update_data(chosen_dormitory=message.text)
+
+    await Support.next()
+    await message.answer('Укажите корпус\. Если нет, поставьте "\-"')
+
+
+@dp.message_handler(state=Support.building)
+async def cmd_building(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_building=message.text)
+    await Support.next()
+    await message.answer("Введите номер комнаты\.")
+
+
+@dp.message_handler(state=Support.room)
+async def cmd_room(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_room=message.text)
+    await Support.next()
+    await message.answer("Укажите, как к Вам обращаться\.")
+
+
+@dp.message_handler(state=Support.name)
+async def cmd_name(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_name=message.text)
+    await Support.next()
+    await message.answer("Введите номер телефона\.")
+
+
+@dp.message_handler(state=Support.phone)
+async def cmd_phone(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_phone=message.text)
+    await Support.next()
+    await message.answer("Укажите свой логин или лицевой счет\.")
+
+
+@dp.message_handler(state=Support.login)
+async def cmd_login(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_login=message.text)
+    await Support.next()
+    await message.answer("Опишите проблему \(подробно\)\.")
+
+
+@dp.message_handler(state=Support.problem)
+async def cmd_problem(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_problem=message.text)
+    await Support.next()
+    await message.answer("В какое время можно перезвонить \(Например: с 15\.00 до 23\.00 или 01\.01\.18 днем\)\.")
+
+
+@dp.message_handler(state=Support.call_time)
+async def cmd_call_time(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_time=message.text)
+
+    await Support.filled.set()
+    await cmd_print(message, state)
+
+
+async def cmd_print(message: types.Message, state: FSMContext):
+    user_data = await state.get_data()
+    await message.answer(f"Полученные данные:\n"
+                         f"Общежитие: {user_data['chosen_dormitory']}\n"
+                         f"Корпус: {user_data['chosen_building']}\n"
+                         f"Комната: {user_data['chosen_room']}\n"
+                         f"Имя: {user_data['chosen_name']}\n"
+                         f"Номер телефона: {user_data['chosen_phone']}\n"
+                         f"Логин: {user_data['chosen_login']}\n"
+                         f"Пробелма: {user_data['chosen_problem']}\n"
+                         f"Время звонка: {user_data['chosen_time']}\n",
+                         reply_markup=InlineKeyboardMarkup(row_width=1).add(keyboards.inline_send,
+                                                                            keyboards.inline_edit,
+                                                                            keyboards.inline_cancel), parse_mode="")
+
+
+@dp.callback_query_handler(text='edit', state=Support.filled)
+async def cmd_edit(call: types.CallbackQuery, state: FSMContext):
+    await cmd_support(call.message)
+
+
+@dp.callback_query_handler(text='send', state=Support.filled)
+async def cmd_send(call: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    if user_data['chosen_dormitory'] in ['ДСВ', 'ДСК', 'ДСШ', 'ДСЯ']:
+        url = DSVKSY_GFORM['url']
+        sending_data = {
+            DSVKSY_GFORM['dorm']: user_data['chosen_dormitory'] + ' ' + user_data['chosen_building'],
+            DSVKSY_GFORM['room']: user_data['chosen_room'],
+            DSVKSY_GFORM['name']: user_data['chosen_name'],
+            DSVKSY_GFORM['login']: user_data['chosen_login'],
+            DSVKSY_GFORM['phone']: user_data['chosen_phone'] + ' from TG_BOT',
+            DSVKSY_GFORM['problem']: user_data['chosen_problem'],
+            DSVKSY_GFORM['time']: user_data['chosen_time']
+        }
+    elif user_data['chosen_dormitory'] == 'ДСЛ':
+        url = DSL_GFORM['url']
+        sending_data = {
+            DSL_GFORM['building_other']: user_data['chosen_building'],
+            DSL_GFORM['building']: '__other_option__',
+            DSL_GFORM['room']: user_data['chosen_room'],
+            DSL_GFORM['name']: user_data['chosen_name'],
+            DSL_GFORM['login']: user_data['chosen_login'],
+            DSL_GFORM['phone']: user_data['chosen_phone'] + ' from TG_BOT',
+            DSL_GFORM['problem']: user_data['chosen_problem'],
+            DSL_GFORM['time']: user_data['chosen_time']
+        }
+    elif user_data['chosen_dormitory'] == 'ФДС':
+        url = FDS_GFORM['url']
+        sending_data = {
+            FDS_GFORM['building_other']: user_data['chosen_building'],
+            FDS_GFORM['building']: '__other_option__',
+            FDS_GFORM['room']: user_data['chosen_room'],
+            FDS_GFORM['name']: user_data['chosen_name'],
+            FDS_GFORM['login']: user_data['chosen_login'],
+            FDS_GFORM['phone']: user_data['chosen_phone'] + ' from TG_BOT',
+            FDS_GFORM['problem']: user_data['chosen_problem'],
+            FDS_GFORM['time']: user_data['chosen_time']
+        }
+    elif user_data['chosen_dormitory'] == 'ГЗ':
+        url = DS_GFORM['url']
+        sending_data = {
+            DS_GFORM['room']: user_data['chosen_building'] + ' ' + user_data['chosen_room'],
+            DS_GFORM['name']: user_data['chosen_name'],
+            DS_GFORM['login']: user_data['chosen_login'],
+            DS_GFORM['phone']: user_data['chosen_phone'] + ' from TG_BOT',
+            DS_GFORM['problem']: user_data['chosen_problem'],
+            DS_GFORM['time']: user_data['chosen_time']
+        }
+    sent = requests.post(url, sending_data)
+    if sent:
+        await call.message.answer('Заявка успешно отправлена\!')
+        await state.finish()
+    else:
+        await call.message.answer('Что-то пошло не так\. Попробуйте еще раз\.')
+        await cmd_print(call.message, state)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+    logging.warning('Bye!')
+
+
+if __name__ == "__main__":
+    # Запуск бота
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
