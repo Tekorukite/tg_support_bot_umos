ad95bc485a8bf42dea8476536a42261740c3451f
Dave Akhmadeev <79203081+Tekorukite@users.noreply.github.com>
2021-10-23 17:02:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -97,16 +97,17 @@ db = psycopg2.connect(
 cur = db.cursor()
 
 
-@dp.message_handler(commands="start")
+@dp.message_handler(commands="start", state='*')
 async def cmd_start(message: types.Message, state: FSMContext):
     await state.finish()
     cur.execute("SELECT * FROM subscribers WHERE tg_user_id = %s;", [message.from_user.id])
-    if len(cur.fetchall()) == 0:
+    row = cur.fetchall()
+    if row is None or len(row) == 0:
         user_data = (message.from_user.first_name, message.from_user.id)
         cur.execute("""INSERT INTO subscribers (name, tg_user_id, reg_date) VALUES(%s, %s, CURRENT_DATE);""", user_data)
         cur.execute("COMMIT;")
         print(f"Added user {user_data[0]} with userid={user_data[1]}")
-    await message.answer(f'Привет, {message.from_user.first_name}\. Я бот техподдержки ЮМОС\. \n'
+    await message.answer(f'Привет, {message.from_user.first_name}. Я бот техподдержки ЮМОС. \n'
                          f'Какой вопрос Вас интересует?',
                          reply_markup=keyboards.start_kb, parse_mode='Markdown')
 
